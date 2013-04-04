3a4e0fe28468eb26c9f10c2c739b92ed1852e95f
Nick Stephens <nick.stephens93@gmail.com>
2013-04-04 16:56:51 -0700

    Added a Writeable class

--- LamAggregator.hs
+++ LamAggregator.hs
@@ -17,6 +17,18 @@ import Text.ParserCombinators.Parsec
 data Alias = Alias String Exp
 	deriving Show
 
+class Writeable a where
+	write :: a -> String
+
+instance Writeable Alias where
+	write (Alias id exp) = id ++ " = " ++ (write exp)
+
+instance Writeable Exp where
+	write (Var v) = "(" ++ "Var" ++ " " ++ v ++ ")"
+	write (App e1 e2) = "(" ++ "App" ++ " " ++ (write e1) ++ " "
+					++ (write e2) ++ ")" 
+	write (Lam v e) = "(" ++ "Lam" ++ " " ++ v ++ " " ++ (write e)
+
 term :: Parser Alias
 term = do
 	ident <- many1 letter 
