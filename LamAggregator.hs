c81aabdefde4d27c7cfd6e513e84534ffc0e47df
Nick Stephens <nick.stephens93@gmail.com>
2013-04-05 01:16:17 -0700

    Now translating lambda expression to valid Haskell

--- LamAggregator.hs
+++ LamAggregator.hs
@@ -3,6 +3,7 @@ module LamAggregator where
 import Lambda
 import LamParsec
 import Text.ParserCombinators.Parsec
+import Data.Char
 
 -- take a .lam file as input
 -- and produce a haskell file containing the Abstract Syntax tree 
@@ -25,15 +26,39 @@ instance Writeable Alias where
 
 instance Writeable Exp where
 	write (Var v) = "(" ++ "Var" ++ " " ++ v ++ ")"
+	write (Cons c) = c
 	write (App e1 e2) = "(" ++ "App" ++ " " ++ (write e1) ++ " "
 					++ (write e2) ++ ")" 
-	write (Lam v e) = "(" ++ "Lam" ++ " " ++ v ++ " " ++ (write e)
+	write (Lam v e) = "(" ++ "Lam" ++ " " ++ v ++ " " ++ (write e) ++ ")"
 
-term :: Parser Alias
-term = do
+-- Alias Parser
+-- alias ::= Letter+ '=' expression
+alias :: Parser Alias
+alias = do
 	ident <- many1 letter 
 	many space
 	char '='
 	many space
 	exp <- expression
+	char ';'
 	return $ Alias ident exp
+
+-- File Aggregation
+-- chop the lambda expressions into distinct aliases
+chop :: Parser [Alias]
+chop = do{ a <- alias
+	 ; many space
+	 ; do{ as <- chop
+	     ; return (a:as)
+	     }
+	   <|> return [a]
+	}
+
+aggregate file = do
+	contents <- readFile file
+	return $ foldl (++) "" $ map ((++"\n") . write) $ run chop contents
+	where output = takeWhile (/= '.') file
+
+-- foldl (++) "" (map ((++"\n") . write) lambda)
+
+lambda = [Alias "zero" (Lam "f" (Lam "x" (Var "x"))), Alias "two" (Lam "f" (Lam "x" (App (Var "f") (App (Var "f") (Var "x")))))] 
