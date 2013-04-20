eabefca4d3ef88272d91870e8f4c4efde68572b9
Nick Stephens <nick.stephens93@gmail.com>
2013-04-19 21:49:41 -0700

    LamSub now working

--- Sub.hs
+++ Sub.hs
@@ -3,26 +3,14 @@ import Lambda
 import LambdaCore
 import LamTranslator
 import Encode
+
+
 lamsub = (App y lamsubrec)
-
-
-
-term = (App (App pr two) (App (App pr (App (App pr three) one)) (App (App pr three) two)))
-
-
-var = (App (App pr three) one)
-
-
-m = (App (App pr three) four)
-
-testsub = (App (App (App lamsub m) var) term)
 lamsubrec = (Lam "f" (Lam "m" (Lam "var" (Lam "term" (App (App lamguard (App (App pr (Var "term")) (App (App pr (Var "var")) (App (App pr (Var "m")) (App (App pr (Var "f")) nil))))) (App (App pr (App (App pr matchvar) varaction)) (App (App pr (App (App pr matchapp) appaction)) (App (App pr (App (App pr matchlam) lamaction)) nil))))))))
 
 
 
-
-
-
+lamguard = (App y (Lam "f" (Lam "i" (Lam "ps" (App (App (App (App frst (App car (Var "ps"))) (Var "i")) (App (App scnd (App car (Var "ps"))) (Var "i"))) (App (App (Var "f") (Var "i")) (App cdr (Var "ps"))))))))
 
 
 
@@ -44,9 +32,9 @@ extractatwo = (Lam "app" (App scnd (App scnd (Var "app"))))
 
 
 
-matchvar = (Lam "terms" (App (App eq three) (App car (Var "terms"))))
-matchlam = (Lam "terms" (App (App eq one) (App car (Var "terms"))))
-matchapp = (Lam "terms" (App (App eq two) (App car (Var "terms"))))
+matchvar = (Lam "terms" (App (App eq three) (App car (App car (Var "terms")))))
+matchlam = (Lam "terms" (App (App eq one) (App car (App car (Var "terms")))))
+matchapp = (Lam "terms" (App (App eq two) (App car (App car (Var "terms")))))
 
 
 
@@ -66,7 +54,3 @@ appaction = (Lam "list" (App (App pr two) (App (App pr (App (App (App (App car (
 vartestlist = (App (App pr (App (App pr three) two)) (App (App pr (App (App pr three) one)) (App (App pr (Var "m")) (App (App pr (Var "f")) nil))))
 lamtestlist = (App (App pr (App (App pr one) (App (App pr three) (App (App pr three) zero)))) (App (App pr (App (App pr three) four)) (App (App pr (Var "m")) (App (App pr (Var "f")) nil))))
 apptestlist = (App (App pr (App (App pr two) (App (App pr one) two))) (App (App pr (App (App pr three) four)) (App (App pr (Var "m")) (App (App pr (Var "f")) nil))))
-
-
-
-lamguard = (App y (Lam "f" (Lam "i" (Lam "ps" (App (App (App (App frst (App car (Var "ps"))) (Var "i")) (App scnd (App car (Var "ps")))) (App (App (Var "f") (Var "i")) (App cdr (Var "ps"))))))))
