69ff7523bd44992eef9c8ad771ba820e73fb361f
Owen Meyer <owenpaulmeyer@gmail.com>
2013-04-27 16:40:00 -0700

    abstract syntax update

--- SECD/PCONS.hs
+++ SECD/PCONS.hs
@@ -1,30 +1,33 @@
 -- Abstract Syntax for PCONS
+module PCONS where
 
+import SEC
 
 type Script = [Def]
 
-data Def = Def Name Func
+data Def = Def Name AFunc
 		deriving (Show, Eq, Ord)
 
-data Func = Cl Args Expr | Rec Args Expr
+data AFunc = Cl Args Expr | Rec Args Expr | TRec Args Expr
 		deriving (Show, Eq, Ord)
 
 type Args = [Expr]
 
 data Expr = App Expr Expr | Lam Name Expr | Var Name | Let Name Expr Expr |
-			Case Expr [(Expr, Expr)] | Cond Expr Expr Expr | UnOp Oper |
-			BinOp Oper | Val Value | Lst [Expr]
+			Case Expr [(Expr, Expr)] | Cond Expr Expr Expr | UnOp Oper Expr |
+			BinOp Oper Expr Expr | Val AVal | Lst [Expr]
 				deriving (Show, Eq, Ord)
 
 type Name = String
 
-data Oper = Add | Sub | Mul | Div | Not | Neg
+--data Oper = Add | Sub | Mul | Div | Mod | Not | Neg
+--		deriving (Show, Eq, Ord)
+
+data AVal = AF Float | AI Int | AC Char | AB Bool
 		deriving (Show, Eq, Ord)
 
-data Value = F Float | I Int | C Char | B Bool
-		deriving (Show, Eq, Ord)
 
 
-
-fac = App (App (Var "fac") (App (App (BinOp Mul) (Var "a")) (Var "n"))) (App (App (BinOp Sub) (Var "n")) (Val (I 1)))
+fac = App (App (Var "fac") (App (App (Lam "a" (Lam "n" (BinOp Mul (Var "a") (Var "n")))) (Var "x")) (Var "y"))) 
+		(App (Lam "a" (BinOp Sub (Var "a") (Val$AI 1))) (Var "x"))
 
